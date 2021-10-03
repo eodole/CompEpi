eb3003b46983a213e9aee554f9298fe362d276e5
Ellie Odole <ellieodole@Ellies-MacBook-Pro-2.local>
2021-10-03 10:16:54 -0500

    updated graphics and got through Vist Freq D Weekday

--- ExploratoryAnalysis.R
+++ ExploratoryAnalysis.R
@@ -11,3 +11,21 @@ results = bind_rows(results, .id = "Sheet")
 
 ggplot(results, mapping = aes(x = `Job Type ID`, y = Estimate )) + geom_point(aes(col = Sheet))
 
+ggplot(results,mapping = aes(x=Estimate))+
+  geom_histogram() +
+  geom_density(aes(kernel = "gaussian"))
+
+results_j3 = filter(results, `Job Type ID` == 3)
+
+df = data.frame(x=1:10)
+jt3 = lapply(setNames(1:7, results_j3$Sheet[1:7]), function(x) dpois(1:10,exp(results_j3$Estimate[x])))
+df = cbind(df, bind_cols(l))
+
+#JT 3 Poisson Plot
+matplot(df[2:8], type = "l", pch =1, col=1:7, ylab = "Density", main = "Poisson Density for Job Type 3")       
+
+
+results_jt12 = filter(results, `Job Type ID` ==12)
+jt12 = lapply(setNames(1:7, results_jt12$Sheet[1:7]), function(x) dpois(1:10, exp(results_jt12$Estimate[x]))) 
+jt12 = bind_cols(jt12)
+matplot(jt12, type = "l", col = 1:7, ylab= "Density", main = "Poisson Density for Job Type 12")
