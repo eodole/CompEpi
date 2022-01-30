1efaade5f44d4898f89ce484001c36c0956f91fa
Leona Odole <eldaleona-odole@uiowa.edu>
2022-01-30 09:36:58 -0600

    updated mcclust, fixed a bug in sythetic day

--- pysim/MCClustSampling.py
+++ pysim/MCClustSampling.py
@@ -30,7 +30,7 @@ def mixtureModel(x ,mu, sigma, prop):
 def generate_sample(mu_list,sigma_list,prop_list):
     sample = None
     while sample == None:
-        x = random.randint(0,15) # superimposed limit of a 4 hour visit 
+        x = random.random() * 5  # x in (0,5) => 0 sec - 4 hour visits are possible.
         y = random.random()
         if y < mixtureModel(x, mu_list, sigma_list,prop_list) :
             sample = x 
--- pysim/syntheticday.py
+++ pysim/syntheticday.py
@@ -15,9 +15,9 @@ import MCClustSampling as ms
 initial_id = 0
 n_rooms = 7
 schedule = []
-#input_staffing[x] = [# of staff of that jtype, lamdba duration, lambda total visits per hour, lambda unique visits ]
+#input_staffing[x] = [# of staff of that jtype, [mu, sigma, prop] duration params, lambda total visits per hour, lambda unique visits ]
 input_staffing = {}
-input_staffing[1] = [2, [[4.4,5.1],[.0007, .008],[.4,.6]], 1.5, 4]
+input_staffing[1] = [2, [[4.4,5.1],[.7, .8],[.4,.6]], 1.5, 4]
 # input_staffing[2] = [1,5,1.5,4]
 # input_staffing[3] = [1, 7,2,4] 
 # input_staffing[4] = [1,8,2.5,4] 
@@ -35,9 +35,9 @@ def room_order(unique_r,total_r):
 
         visited = rooms[0:unique_r]
         first_n_rooms = len(visited)
-        #print(first_n_rooms)
+        
         for i in range(0, total_r-unique_r):
-            idx = random.randint(0,first_n_rooms)
+            idx = random.randint(0,first_n_rooms-1)
             visited.append(visited[idx])
     #Otherwise repeat the same room 
     else:
@@ -59,7 +59,9 @@ def assign_visit_dur(hid, jtid,staff_info, rid):
     sigma = staff_info[1][1]
     prop = staff_info[1][2]
     #Sample mcclust for duration time
-    duration = ms.generate_sample(mu,sigma,prop) ## This needs to be fixed 
+    #duration = random.random()
+    duration = ms.generate_sample(mu,sigma,prop) ## This needs to be fixed ... how
+
 
     return [hid, jtid, rid, duration]
 
@@ -88,49 +90,3 @@ for jtid in input_staffing.keys():
             #Generate duration using hid, jtid, visits given 
             schedule.append(assign_visit_dur(hid,jtid, staff_info,visited[i]))
             print(schedule[-1])
-
-#For each healthcare worker
-# Sum up the time that they're spending patient rooms,      
-        
-
-
-
-# #Establish the number of patient rooms being simulated
-# n_pat_rooms = int(sys.argv[1])
-# #Establist num of hcw being simulated 
-# nhid = int(sys.argv[2]) 
-
-# begin_date =  datetime.strptime(sys.argv[3], "%Y/%M/%d") 
-
-# print(n_pat_rooms, nhid, begin_date)
-
-# rid = np.arange(1,n_pat_rooms)
-# hid = np.arange(100,100+nhid)
-
-
-
-# #Create large csv file 
-# col_names = ['rid','hid','duration']  #["rid,hid,duration,itime,otime,idisp,odisp,jtid,jtname"]
-# output_csv = pd.DataFrame(columns=col_names)
-
-# def generate_row():
-#     global rid
-#     global hid
-#     r1 = random.randint(0,len(rid)-1)
-#     r2 = random.randint(0,len(hid)-1)
-#     d1 = generate_sample(1.1) #### Needs to be changed to generate a sample from mclust
-#     return {'rid':rid[r1], 'hid': hid[r2],'duration': d1 }
-
-
-# h = 0 
-# for hour in range(0,24):
-#     for i in range(0,generate_sample(1.1)): ## Need to generate the Poi num of visits
-         
-#         output_csv = output_csv.append(generate_row(), ignore_index=True)
-#     #Take each healthcare worker
-#     #Calculate how many time they visit rooms
-#     #randomly select room to visit
-#     #Caluclate duration of vist
-#     #Add row to data table 
-
-# print(output_csv)
