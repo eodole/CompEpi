86eec3d6b5a319a5276746dd0a3838a1e26caf50
Leona Odole <eldaleona-odole@uiowa.edu>
2022-02-03 14:00:15 -0600

    Added comments and clarification on how the script works

--- pysim/syntheticday.py
+++ pysim/syntheticday.py
@@ -3,8 +3,6 @@
 #rid,hid,duration,itime,otime,idisp,odisp,jtid,jtname
 
 import sys
-from turtle import st
-from importlib_metadata import unique_everseen
 import numpy as np
 from datetime import date, datetime
 import random
@@ -52,10 +50,7 @@ input_staffing[1] = [1, [[4.5,5.5],[0.03,0.75],[.5,.5]], 4,3.5 ] #7
 
 #Returns a list of numbers that represent the rids and order that they are visited in
 def room_order(unique_r,total_r):
-    # global n_rooms
-    # #Shuffle the rooms randomly 
-    # rooms = list(range(0,n_rooms))
-    # random.shuffle(rooms)
+    
     visited = []
     #If there is >1 unique room pick from the first 
     #n rooms with replacement to choose the next (total -n) rooms
@@ -69,18 +64,7 @@ def room_order(unique_r,total_r):
 
     return visited
         
-        # first_n_rooms = len(visited)
-        # visited = unique_r[0:unique_r]
-
-    #     for i in range(0, total_r-unique_r):
-    #         idx = random.randint(0,first_n_rooms-1)
-    #         visited.append(visited[idx])
-    # #Otherwise repeat the same room 
-    # else:
-    #     visited = [rooms[0]] * total_r
-        
-    # return visited
-    
+  
 
 #Assign an hid number to the healthcare worker 
 def assign_hid():
@@ -95,27 +79,33 @@ def assign_visit_dur(staff_info):
     sigma = staff_info[1][1]
     prop = staff_info[1][2]
     #Sample mcclust for duration time
-    duration = ms.generate_sample(mu,sigma,prop) ## This needs to be fixed ... how
-    #otime = itime + datetime.timedelta(seconds=duration)
-
+    duration = ms.generate_sample(mu,sigma,prop) 
     return duration
 
+
+# Visits are generate in an hourly fashion 
+# Where each hour is returned as a list of visit tuples 
+# Each tuple represents a healthcare worker visiting a room
+# Parameters for each tuple are hid,jtid,duration, intime, outtime 
 def generate_visits(hid,jtid,staff_info,visited,itime):
-    #print(hid,jtid,visited,itime)
     
     durations = []
     synthetic_v = []
-    #print(type(visited),visited)
+    
+    # Generate visit length for each visit in that hour
     for i in range(0,len(visited)):
-        # itimes.append(random_date(itime))
         durations.append(assign_visit_dur(staff_info))
-        #print(visited)
+    
+    # Find the amount of time not being used in visits 
     not_visit_time = (60*60-sum(durations))/len(visited)
-    # print(len(visited),not_visit_time)
+   
+    # Find the itimes using the non_visit time information to 
+    # properly space the visits
     itimes = random_date(itime,not_visit_time,len(visited))
+    
+    #Put everything together and append each tuple to synthetic visits for that hour
     for i in range(0,len(itimes)):
-        intime = itimes[i]
-        
+        intime = itimes[i] 
         dur = durations[i]
         otime = intime + datetime.timedelta(seconds=dur)
         rid = visited[i]
@@ -123,14 +113,9 @@ def generate_visits(hid,jtid,staff_info,visited,itime):
     return(synthetic_v)
 
         
-    #itimes = sorted(itimes)## This doesn't work how do i get it to work
-    #print(itimes)
 
-def total_visits_shift(lam): #,shift_len):
+def total_visits_shift(lam): 
     #lam = lam per hour
-    #shift_len in hours 
-    
-    # for i in range(0,shift_len):
     total_v= ps.generate_sample(lam)
     return total_v
 
@@ -148,8 +133,8 @@ def random_date(start,not_visit_time,n_visits):
 
 #print(random_date(start_date, end_date))
 
-#For each job type...
 if __name__ == "__main__":
+    #For each job type...
     for jtid in input_staffing.keys():
         staff_info = input_staffing[jtid]
         #Generate n staff...
@@ -161,31 +146,21 @@ if __name__ == "__main__":
             random.shuffle(unq_r)
             unq_r = unq_r[0: (ps.generate_sample(staff_info[3])-1) ]
             print(unq_r)
+            #Start at hour 0 of the day 
             for h in range(0,12):
-                #total_v = total_visits_shift(staff_info[2],12)
+                #Find the total num visits made in that hour
                 total_v = total_visits_shift(staff_info[2])
-                # print(total_v, unq_r)
+                
+                #If num rooms are visited then visited == empty
                 if total_v > 0: 
                     visited = room_order(unq_r,total_v)
                 else:
                     visited = []
                 
+                #If there are visits made in that hour, generate them 
                 if len(visited) > 0:
                     schedule.append(generate_visits(hid,jtid,staff_info,visited,start_date + datetime.timedelta(hours=h)))
     print(schedule)
                 
-                #Generate duration using hid, jtid, visits given 
-                
-                # for visits in range(0,total_v[h]):
-                #     itime = random_date(start_date + datetime.timedelta(hours = h))#, start_date + datetime.timedelta(hours=1+h))
-                #     schedule.append(assign_visit_dur(hid,jtid, staff_info,visited[visits],itime))
-                #     print(schedule[-1])
-                # for visits in range(0,total_v[h]):
-                #     visited.pop(0)
+               
             
-            
-            
-                #print(visited,visits)
-            #     print(itime)
-            #schedule.append(assign_visit_dur(hid,jtid, staff_info,visited[i]))
-            #print(schedule[-1])
