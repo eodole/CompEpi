6f8b16a7a794821b56f2d23ad738e659c98650fb
Leona Odole <eldaleona-odole@uiowa.edu>
2022-12-26 09:01:51 +0100

    reorganization, upload of HT

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Project Purpose
+## Repository Background
 
 __What is CompEpi?__
 
@@ -10,10 +10,17 @@ More about the CS Department: [https://cs.uiowa.edu/research]
 
 __Description__
 
-This repository contains all scripts used for the writing of my honors thesis. It also includes the parameters produced which may be of use to other modelers. When using the generative distributions please cite this repository. 
+This repository contains all code used in the writing of my honors thesis. It also includes supporting documents and parameters which may be of use to other modelers. When using the generative distributions please cite this repository. 
+
+## Modeling Healthcare Worker Behaviors From Locator Traces 
+
+__Abstract__
+
+Every year approximately two million people in America are affected by healthcare-associated infections (HAIs), with nearly 75,000 of those infections resulting in death. The treatment of HAIs adds nearly $500 million to US healthcare costs per year. Despite good hand hygiene being the most effective preventive measure, compliance among HCWs still remains low. While there are possible alternative interventions to reduce HAIs—e.g. alternative patient room assignment strategies, changes to hospital infrastructure, vaccination mandates, increased personal protective equipment—implementing them is disruptive and expensive. Therefor our research instead aims to provide a simulation environment that will allow researchers to compare the effectiveness of such interventions in simulation before implementation in the clinical setting.
 
 __Supporting Documents__
 
+- Honors Thesis 
 - Job Type Encoding Schema
 - Facility Groups
 
--- simulation/utilities/MCClustSampling.py
+++ simulation/utilities/MCClustSampling.py
@@ -1,23 +1,29 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 12 11:14:56 2021
+# Copyright 2022, The University of Iowa.  All rights reserved.
+# Permission is hereby given to use and reproduce this software 
+# for non-profit educational purposes only.
 
-@author: ellieodole
-"""
+# REU Computing for Health and Well-being 2022
+# Partial Funding from CDC MiND Grant and NSF REU Grant
+# Authors: Leona Odole, Maren Gingerich, Amanda Perera
 
-import random 
+# Functions Avaiable
+#   - readMclust : which accepts mclust output csv file and stores parameters in a data frame
+#   - generate_sample: randomly generates a duration of visit in seconds from a mixture model
+#   - normal_dist: finds the probability for a normal distribution at a given point x
+#   - mixtureModel: finds the probability for a mixture model at a given point x
+#   - grab_parameters: create a list of parameters that can be used as input for generate_sample function
+
+import pandas as pd 
+import random
 import numpy as np
-import sys
-import ast
-import math
 
-#Find the probability for a single normal distribution
+
+# Find the probability for a single normal distribution
 def normal_dist(x , mean , sd):
     prob_density = (1/(sd*np.sqrt(2*np.pi))* np.exp((-1/2)*(((x - mean)/sd)**2)))
     return prob_density
 
-#Find the probability as a point for the mixture model
+# Find the probability at a point for the mixture model
 def mixtureModel(x ,mu, sigma, prop): 
     prob_density = 0
     for i in range(0, len(mu)):
@@ -26,43 +32,61 @@ def mixtureModel(x ,mu, sigma, prop):
    
     return prob_density
 
-#Randomly generate a sample from the mixture model 
+# Input: filename of csv file containing Mclust data by jtid
+# Output: dataframe indexed by jtid
+def readMclust(filename):
+    df = pd.read_csv(filename, index_col='jtid')
+    return df
+
+
+
+def prepend(list, str):
+    str += '{0}'
+    list = [str.format(i) for i in list]
+    return list
+
+# Create a list of parameters that can be used as input for generate_sample function 
+# Inputs: dataframe from readMclust function and jtid specified by user
+# Outputs: list of all mu, var, and prop for each cluster for that jtid 
+def grab_parameters(frame, ftid, utid, jtid):
+    new = frame[(frame['jtid'] == jtid) & (frame['ftid'] == ftid) & (frame['utid'] == utid)]
+    nclust = int(new['num_dists'])
+    lst = list(range(1,nclust + 1))
+    pro = prepend(lst, 'pro ')
+    mean = prepend(lst, 'mean ')
+    sigmasq = prepend(lst, 'sigmasq ')
+    mu = []
+    var = []
+    prop = []
+    for i in range(0, len(lst)):
+        a = list(new[mean[i]])
+        mu.append(a)
+        b = list(new[sigmasq[i]])
+        var.append(b)
+        c = list(new[pro[i]])
+        prop.append(c)
+
+    return mu, var, prop
+    
+
+
+# Input: mu_list, sigma_list, prop_list, lists of parameters for an mclust mixture model 
+#       (might change this to accept dataframe instead.)
+# Output: a randomly generated observation based on the input mixutre model, using rejection sampling: 
+#           for more information on rejection sampling, see: https://en.wikipedia.org/wiki/Rejection_sampling
+
 def generate_sample(mu_list,sigma_list,prop_list):
     sample = None
     while sample == None:
-        x = random.random() * 8 # ln(3600sec) ~ 8.18 => limiting the visit length to just under 4 hours 
+        #Randomly generate a sample from the mixture model using rejection sampling from  :
+            # 1. Sample a point on the x-axis from the proposal distribution.
+            # 2. Draw a vertical line at this x-position, up to the maximum y-value of the probability density function of the proposal distribution.
+            # 3. Sample uniformly along this line from 0 to the maximum of the probability density function. If the sampled value is greater than the 
+            # value of the desired distribution at this vertical line, reject the x-value and return to step 1; else the x-value is a sample from 
+            # the desired distribution.
+        x = random.random() * 60 * 60 * 4 # limiting length to 4 hrs 
         y = random.random()
         if y < mixtureModel(x, mu_list, sigma_list,prop_list) :
             sample = x 
-    return math.exp(sample) 
+    return sample
 
-## DEFUNT: 
-
-# def generate_sample(n):
-#     n_samples =[]
-#     while len(n_samples) < n:
-#         x = random.randint(0, maxSec) 
-#         y = random.random()
-        
-#         if y < mixtureModel(x):
-            
-#             n_samples.append(x)
-#     return n_samples
-
-
-
-# if __name__ == "__main__":
-#     #Takes inputs as :
-#     # proportion list "[p1,p2....pn]""
-#     # mean list "[m1,m2,....,m_n]""
-#     # standard deviation list "[s1,s2,....,sn]"
-#     prop = ast.literal_eval(sys.argv[1])
-#     print(prop)
-#     print(type(prop))
-#     mu = ast.literal_eval(sys.argv[2])
-#     sigma = ast.literal_eval(sys.argv[3])
-#     n = ast.literal_eval(sys.argv[4])
-#     print(generate_samples(n))
-    
-
-    
